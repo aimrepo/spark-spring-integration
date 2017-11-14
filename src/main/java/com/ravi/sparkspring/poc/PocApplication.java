ba9d69c095ecaea544e3bd8679e0fb59486fe0b7
559173 <lakshumanan.kesavan@cognizant.com>
2017-11-14 11:25:20 +0530

    Test -11:25

--- src/main/java/com/ravi/sparkspring/poc/PocApplication.java
+++ src/main/java/com/ravi/sparkspring/poc/PocApplication.java
@@ -10,14 +10,14 @@ import com.ravi.sparkspring.poc.job.WordCountJob;
 @SpringBootApplication
 public class PocApplication /*implements CommandLineRunner*/ {
 
-    //@Autowired
+    //@Autowired 
     private WordCountJob wordCountJob;
     
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 	}
 	
-	
+	//Testing code checkin
 
 /*    @Override
     public void run(String... args) throws Exception {
