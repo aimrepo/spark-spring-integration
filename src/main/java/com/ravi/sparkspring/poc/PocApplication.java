9b92703fcbe8915e5a1fc2897db8834e1b945c03
aimrepo <lakshumanan13@gmail.com>
2017-12-12 09:22:55 +0530

    TestCommit at 9.22AM

--- src/main/java/com/ravi/sparkspring/poc/PocApplication.java
+++ src/main/java/com/ravi/sparkspring/poc/PocApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.ravi.sparkspring.poc.job.WordCountJob;
 
 @SpringBootApplication
-public class PocApplication /*implements CommandLineRunner*/ {
+public class PocApplication /*implements CommandLineRunner interface*/ {
 
     //@Autowired
 	private WordCountJob wordCountJob;
